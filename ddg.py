89c2afb3b75715182c59cfe5c1acdc1cf0f96ad2
DAVIDO <davidjoliver86@gmail.com>
2014-02-02 10:52:07 -0800

    simple search

--- ddg.py
+++ ddg.py
@@ -2,3 +2,8 @@ from selenium import webdriver
 
 driver = webdriver.Firefox()
 driver.get('http://www.duckduckgo.com')
+
+# search for bacon
+search_box = driver.find_element_by_id('search_form_input_homepage')
+search_box.send_keys('bacon')
+driver.find_element_by_id('search_button_homepage').click()
