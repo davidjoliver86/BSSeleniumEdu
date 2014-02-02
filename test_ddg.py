cd884ed38e85c278849213df03e0a9464f36a352
DAVIDO <davidjoliver86@gmail.com>
2014-02-02 11:06:33 -0800

    lets make a test out of this search

---
+++ test_ddg.py
@@ -0,0 +1,20 @@
+import unittest
+from selenium import webdriver
+
+
+class TestDuckDuckGo(unittest.TestCase):
+
+    def test_duck_duck_go(self):
+        driver = webdriver.Firefox()
+        driver.get('http://www.duckduckgo.com')
+
+        # search for bacon
+        search_box = driver.find_element_by_id('search_form_input_homepage')
+        search_box.send_keys('bacon')
+        driver.find_element_by_id('search_button_homepage').click()
+
+        # look for a description
+        self.assertTrue('Bacon is evil' in driver.find_element_by_tag_name('body').text)
+
+if __name__ == '__main__':
+    unittest.main()
