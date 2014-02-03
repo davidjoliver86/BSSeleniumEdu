c9f9a3fcec5f995a0a18d3cd25ea310a5cb671cb
DAVIDO <davidjoliver86@gmail.com>
2014-02-02 22:19:46 -0800

    wrap up

--- test_pproc.py
+++ test_pproc.py
@@ -28,19 +28,31 @@ class TestPlanalyzerRating(unittest.TestCase):
         driver.find_element_by_css_selector('input[value="Search"]').click()
         driver.find_element_by_id('result_list').find_element_by_tag_name('tbody').find_element_by_tag_name('a').click()
 
-    def get_planalyzer_rating(self):
+    def get_planalyzer_rating_link(self):
         """
         The link text is the BrightScope rating rounded to 0.1.
         """
         driver = self.driver
         driver.find_element_by_link_text('View Planalyzer').click()
+        driver.switch_to_window(driver.window_handles[-1])
         link = driver.find_element_by_id('planalyzer-rating').find_element_by_tag_name('a')
         return link
 
     def test_planalyzer_rating(self):
         self.login()
         self.search_for_plan_admin('in out burger')
-        pr_link = self.get_planalyzer_rating()
+
+        # get rating from planalyzer
+        pl_link = self.get_planalyzer_rating_link()
+        pl_rating = float(pl_link.text)
+
+        # get rating from rating page
+        pl_link.click()
+        self.assertTrue('401k Rating' in self.driver.title)
+        rp_rating = int(self.driver.find_element_by_id('your_plan_rating').text)
+
+        # compare ratings
+        self.assertTrue(abs(pl_rating - rp_rating) <= 0.5)
 
     def tearDown(self):
         self.driver.quit()
